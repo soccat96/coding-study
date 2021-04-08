e1e3b7723b689701abce80bbf47b8681e154c17f
soc <soc@epinet.kr>
2021-04-08 10:12:35 +0900

    first commit

---
+++ src/kr/soc/baekjoon/Problem10171_java.java
@@ -0,0 +1,10 @@
+package kr.soc.baekjoon;
+
+public class Problem10171_java {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}

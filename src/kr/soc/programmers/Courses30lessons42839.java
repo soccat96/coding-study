25e339fbc408150e65df212eefd5d113e40cdfec
soc <soc@epinet.kr>
2021-05-04 18:29:12 +0900

    fifth meeting - java

---
+++ src/kr/soc/programmers/Courses30lessons42839.java
@@ -0,0 +1,87 @@
+package kr.soc.programmers;
+
+import java.util.*;
+
+public class Courses30lessons42839 {
+    public int solution(String numbers) {
+        TreeSet<Integer> numberSet = new TreeSet<>();
+        char[] numberChars = numbers.toCharArray();
+
+        for(int a=0; a<numberChars.length; a++) {
+            String as = String.valueOf(numberChars[a]);
+            numberSet.add(Integer.parseInt(as));
+
+            for(int b=0; b<numberChars.length; b++) {
+                if(a == b) { continue; }
+                String bs = as + numberChars[b];
+                numberSet.add(Integer.parseInt(removeFrontZero(bs)));
+
+                for(int c=0; c<numberChars.length; c++) {
+                    if(c == a || c == b) { continue; }
+                    String cs = bs + numberChars[c];
+                    numberSet.add(Integer.parseInt(removeFrontZero(cs)));
+
+                    for(int d=0; d<numberChars.length; d++) {
+                        if(d == a || d == b || d == c) { continue; }
+                        String ds = cs + numberChars[d];
+                        numberSet.add(Integer.parseInt(removeFrontZero(ds)));
+
+                        for(int e=0; e<numberChars.length; e++) {
+                            if(e == a || e == b || e == c || e == d) { continue; }
+                            String es = ds + numberChars[e];
+                            numberSet.add(Integer.parseInt(removeFrontZero(es)));
+
+                            for(int f=0; f<numberChars.length; f++) {
+                                if(f == a || f == b || f == c || f == d || f == e) { continue; }
+                                String fs = es + numberChars[f];
+                                numberSet.add(Integer.parseInt(removeFrontZero(fs)));
+
+                                for(int g=0; g<numberChars.length; g++) {
+                                    if(g == a || g == b || g == c || g == d || g == e || g == f) { continue; }
+                                    String gs = fs + numberChars[g];
+                                    numberSet.add(Integer.parseInt(removeFrontZero(gs)));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        int cnt = 0;
+        for(Integer x : numberSet) {
+            if(isPrimeNumber(x)) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+    public String removeFrontZero(String number) {
+        if(number.length() <= 0) {
+            return "0";
+        }
+
+        char first = number.charAt(0);
+        if(first == '0') {
+            number = removeFrontZero(number.substring(1));
+        }
+
+        return number;
+    }
+
+    public boolean isPrimeNumber(int number) {
+        if(number == 0 || number == 1) {
+            return false;
+        }
+
+        for(int i=2; i<number; i++) {
+            if(number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ test/kr/soc/programmers/Courses30lessons42839Test.java
@@ -0,0 +1,55 @@
+package kr.soc.programmers;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Courses30lessons42839Test {
+
+    @Test
+    void isPrimeNumber() {
+        Courses30lessons42839 c = new Courses30lessons42839();
+        assertEquals(false, c.isPrimeNumber( 0));
+        assertEquals(false, c.isPrimeNumber( 1));
+        assertEquals(true,  c.isPrimeNumber( 2));
+        assertEquals(true,  c.isPrimeNumber( 3));
+        assertEquals(false, c.isPrimeNumber( 4));
+        assertEquals(true,  c.isPrimeNumber( 5));
+        assertEquals(false, c.isPrimeNumber( 6));
+        assertEquals(true,  c.isPrimeNumber( 7));
+        assertEquals(false, c.isPrimeNumber( 8));
+        assertEquals(false, c.isPrimeNumber( 9));
+        assertEquals(false, c.isPrimeNumber(10));
+        assertEquals(true,  c.isPrimeNumber(11));
+        assertEquals(false, c.isPrimeNumber(12));
+        assertEquals(true,  c.isPrimeNumber(13));
+        assertEquals(false, c.isPrimeNumber(14));
+        assertEquals(false, c.isPrimeNumber(15));
+        assertEquals(false, c.isPrimeNumber(16));
+        assertEquals(true,  c.isPrimeNumber(17));
+        assertEquals(false, c.isPrimeNumber(18));
+        assertEquals(true,  c.isPrimeNumber(19));
+        assertEquals(false, c.isPrimeNumber(20));
+        assertEquals(true,  c.isPrimeNumber(23));
+    }
+
+    @Test
+    void removeFrontZero() {
+        Courses30lessons42839 c = new Courses30lessons42839();
+        assertEquals("12345", c.removeFrontZero("012345"));
+        assertEquals("12345", c.removeFrontZero("000012345"));
+        assertEquals("102000300405", c.removeFrontZero("0000102000300405"));
+        assertEquals("12345000", c.removeFrontZero("000012345000"));
+        assertEquals("0", c.removeFrontZero("0000000"));
+    }
+
+    @Test
+    void solution() {
+        Courses30lessons42839 c = new Courses30lessons42839();
+        assertEquals(3, c.solution("17"));
+        assertEquals(2, c.solution("011"));
+    }
+}

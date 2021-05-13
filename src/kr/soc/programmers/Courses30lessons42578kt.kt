08e2526df50f756d4b1c443cc00a6ac6a0bf37f0
soc <soc@epinet.kr>
2021-05-13 18:07:06 +0900

    sixth meeting - kotlin

---
+++ src/kr/soc/programmers/Courses30lessons42578kt.kt
@@ -0,0 +1,21 @@
+package kr.soc.programmers
+
+class Courses30lessons42578kt {
+    fun solution(clothes: Array<Array<String>>): Int {
+        var answer = 1
+        val map = mutableMapOf<String, MutableList<String>>()
+
+        clothes.forEach {
+            if(map[it[1]].isNullOrEmpty()) map[it[1]] = mutableListOf<String>()
+            map[it[1]]!!.add(it[0])
+        }
+
+        map.keys.forEach { answer *= map[it]!!.size + 1 }
+
+        return answer - 1
+    }
+
+//    fun solution(clothes: Array<Array<String>>): Int {
+//        return clothes.groupBy { it[1] }.values.fold(1) { acc, v -> acc * (v.size + 1) }  - 1
+//    }
+}

87944b5ae6fcfb50267d8522f6b08413af2b6dc6
soc <soc@epinet.kr>
2021-05-31 19:14:36 +0900

    ninth meeting

---
+++ test/kr/soc/programmers/Courses30lessons62048Test.java
@@ -0,0 +1,14 @@
+package kr.soc.programmers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Courses30lessons62048Test {
+
+    @Test
+    void solution() {
+        Courses30lessons62048 c = new Courses30lessons62048();
+        assertEquals(80, c.solution(8, 12));
+    }
+}
---
+++ test/kr/soc/programmers/Courses30lessons62048ktTest.kt
@@ -0,0 +1,13 @@
+package kr.soc.programmers
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class Courses30lessons62048ktTest {
+
+    @Test
+    fun solution() {
+        assertEquals(80, Courses30lessons62048kt().solution(8, 12))
+    }
+}
